fcbe44ada58522e3284e1753978c791319865fe1
Andreas Alfons <alfons@ese.eur.nl>
2021-05-13 12:04:14 +0200

    cleaned up files for unit tests

--- inst/tests/test-fastLasso-highdim.R
+++ inst/tests/test-fastLasso-highdim.R
@@ -19,33 +19,33 @@ y <- c(x %*% beta + rnorm(n))        # response
 ## run tests
 
 test_that("different values for lambda yield correct solution", {
-  
+
   ## fit lasso with lars() as reference solution
   fitLars <- lars(x, y, type="lasso")
-  
+
   ## extract values of lambda according to parametrization in robustHD
   lambda <- 2 * fitLars$lambda / n
   sMax <- length(lambda)
-  
+
   ## choose different values of lambda and check solutions
-  lambda <- c(lambda[1] * 1.5, 
-              sort.int(union(lambda, (lambda[-sMax] + lambda[-1]) / 2), 
-                       decreasing=TRUE), 
+  lambda <- c(lambda[1] * 1.5,
+              sort.int(union(lambda, (lambda[-sMax] + lambda[-1]) / 2),
+                       decreasing=TRUE),
               lambda[sMax] * 0.5, 0.00001, 0)
-  
+
   ## extract coefficients from solution computed via lars()
   coefLars <- sapply(n*lambda/2, function(l) {
     beta <- coef(fitLars, s=l, mode="lambda")
     alpha <- fitLars$mu - sum(beta * fitLars$meanx)
     c(alpha, beta)
   })
-  
+
   ## fit models with fastLasso() and extract coefficients
   coefFastLasso <- sapply(lambda, function(l) {
     fitFastLasso <- robustHD:::fastLasso(x, y, lambda=l)
     coef(fitFastLasso)
   })
-  
+
   ## test whether coefficients are equal
   expect_equal(coefLars, coefFastLasso)
 })
--- inst/tests/test-fastLasso-multiple.R
+++ inst/tests/test-fastLasso-multiple.R
@@ -19,48 +19,48 @@ y <- c(x %*% beta + rnorm(n))  # response
 ## run tests
 
 test_that("special case for no penalty yields LS solution", {
-  
+
   ## compute LS solution and extract coefficients
   fitLS <- lm(y~x)
   coefLS <- unname(coef(fitLS))
-  
+
   ## fit models with fastLasso() and extract coefficients
   fitFastLasso <- robustHD:::fastLasso(x, y, lambda=0)
   coefFastLasso <- coef(fitFastLasso)
-  
+
   ## test whether coefficients are equal
   expect_equal(coefLS, coefFastLasso)
 })
 
 
 test_that("different values for lambda yield correct solution", {
-  
+
   ## fit lasso with lars() as reference solution
   fitLars <- lars(x, y, type="lasso")
-  
+
   ## extract values of lambda according to parametrization in robustHD
   lambda <- 2 * fitLars$lambda / n
   sMax <- length(lambda)
-  
+
   ## choose different values of lambda and check solutions
-  lambda <- c(lambda[1] * 1.5, 
-              sort.int(union(lambda, (lambda[-sMax] + lambda[-1]) / 2), 
-                       decreasing=TRUE), 
+  lambda <- c(lambda[1] * 1.5,
+              sort.int(union(lambda, (lambda[-sMax] + lambda[-1]) / 2),
+                       decreasing=TRUE),
               lambda[sMax] * 0.5, 0.00001)
-  
+
   ## extract coefficients from solution computed via lars()
   coefLars <- sapply(n*lambda/2, function(l) {
     beta <- coef(fitLars, s=l, mode="lambda")
     alpha <- fitLars$mu - sum(beta * fitLars$meanx)
     c(alpha, beta)
   })
-  
+
   ## fit models with fastLasso() and extract coefficients
   coefFastLasso <- sapply(lambda, function(l) {
     fitFastLasso <- robustHD:::fastLasso(x, y, lambda=l)
     coef(fitFastLasso)
   })
-  
+
   ## test whether coefficients are equal
   expect_equal(coefLars, coefFastLasso)
 })
